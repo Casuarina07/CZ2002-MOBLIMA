015db758de8d18c894680a8780b9fd9cc1a8a29d
davidhaoz <davidhaoz@live.com>
2019-11-02 14:16:21 +0800

    Update Ticket.java
    Updated Ticket class

--- MOBLIMA/src/entity/Ticket.java
+++ MOBLIMA/src/entity/Ticket.java
@@ -8,11 +8,12 @@ public class Ticket {
 	private double standardPrice, finalPrice;
 	private boolean isPublicHoliday, isWeekday;
 
-	public Ticket(String movieName, String movieType, String cinemaType, double standardPrice) {
+	public Ticket(String movieName, String movieType, String cinemaType, String seatID, double standardPrice) {
 		super();
 		this.movieName = movieName;
 		this.movieType = movieType;
 		this.cinemaType = cinemaType;
+		this.seatID = seatID;
 		this.standardPrice = standardPrice;
 		this.isPublicHoliday = isPublicHoliday();
 		this.isWeekday = isWeekday();
@@ -28,6 +29,10 @@ public class Ticket {
 
 	public String getCinemaType() {
 		return cinemaType;
+	}
+
+	public String getSeatID() {
+		return seatID;
 	}
 
 	public double getStandardPrice() {
@@ -58,6 +63,7 @@ public class Ticket {
 		// Retrieve system's date
 		LocalDate date = LocalDate.now();
 
+		// Returns false for weekend, true for weekday
 		if (date.getDayOfWeek().getValue() + 1 == Calendar.SATURDAY
 				|| date.getDayOfWeek().getValue() + 1 == Calendar.SUNDAY)
 			return false;
