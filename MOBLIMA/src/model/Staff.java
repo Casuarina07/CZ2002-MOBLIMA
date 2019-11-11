16d826a87c96263440b5c167e8d3b959fc94d4a4
gwynbxm <45753405+gwynbxm@users.noreply.github.com>
2019-11-11 21:25:58 +0800

    Updated Staff.java
    Added in Arraylist of staff and the necessary setters and constructors.

--- MOBLIMA/src/model/Staff.java
+++ MOBLIMA/src/model/Staff.java
@@ -1,14 +1,39 @@
 package model;
 
+import java.util.ArrayList;
+
+
+/**
+ * @author Gwyn Bong Xiao Min
+ * @since 2019-11-07
+ */
 public class Staff {
 
 	private String staffID, staffName, staffPwd;
+	private static ArrayList<Staff> staffList = new ArrayList<>();
 
 	public Staff(String staffID, String staffName, String staffPwd) {
 		super();
 		this.staffID = staffID;
 		this.staffName = staffName;
 		this.staffPwd = staffPwd;
+	}
+	
+	public Staff(String staffName, String staffPwd) {
+		super();
+		this.staffName = staffName;
+		this.staffPwd = staffPwd;
+	}
+
+	public Staff() {
+	}
+	
+	/**
+	 * 
+	 * @return A list of Staff users
+	 */
+	public static ArrayList<Staff> getStaffList() {
+		return staffList;
 	}
 
 	public String getStaffID() {
@@ -22,4 +47,18 @@ public class Staff {
 	public String getStaffPwd() {
 		return staffPwd;
 	}
+
+	public void setStaffID(String staffID) {
+		this.staffID = staffID;
+	}
+
+	public void setStaffName(String staffName) {
+		this.staffName = staffName;
+	}
+
+	public void setStaffPwd(String staffPwd) {
+		this.staffPwd = staffPwd;
+	}
+	
+	
 }
