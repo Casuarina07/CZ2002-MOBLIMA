6aa6371c43cf9f8f7969badbe2ed9cb65dfb92cb
davidhaoz <davidhaoz@live.com>
2019-11-14 18:08:29 +0800

    Updated Cinema
    Updated Cinema.java & created Cinema.csv

--- MOBLIMA/src/model/Cinema.java
+++ MOBLIMA/src/model/Cinema.java
@@ -7,33 +7,38 @@ import java.util.ArrayList;
  * @author Gwyn Bong Xiao Min
  * @since 2019-10-24
  */
-public class Cinema extends Cineplex {
-	String cinemaID, cinemaCode, cinemaName;
+public class Cinema {
+	String cinemaCode, cinemaName;
 	private ArrayList<MovieListing> movieList;
 
-
-	public Cinema(String cineplexID, String cineplexName, ArrayList<Cinema> cinemaList, String cinemaID,
-			String cinemaCode, String cinemaName, ArrayList<MovieListing> movieList) {
-		super(cineplexID, cineplexName, cinemaList);
-		this.cinemaID = cinemaID;
+	public Cinema(String cinemaCode, String cinemaName, ArrayList<MovieListing> movieList) {
+		super();
 		this.cinemaCode = cinemaCode;
 		this.cinemaName = cinemaName;
 		this.movieList = movieList;
 	}
 
-	public String getCinemaID() {
-		return cinemaID;
+	public Cinema(String cinemaCode, String cinemaName) {
+		super();
+		this.cinemaCode = cinemaCode;
+		this.cinemaName = cinemaName;
+	}
+
+	public String getCinemaCode() {
+		return cinemaCode;
 	}
 
 	public String getCinemaName() {
 		return cinemaName;
 	}
 
-	public void setCinemaID(String cinemaID) {
-		this.cinemaID = cinemaID;
+	public ArrayList<MovieListing> getMovieList() {
+		return movieList;
 	}
 
-	public void setCinemaName(String cinemaName) {
-		this.cinemaName = cinemaName;
+	public Cinema(ArrayList<MovieListing> movieList) {
+		super();
+		this.movieList = movieList;
 	}
+
 }
