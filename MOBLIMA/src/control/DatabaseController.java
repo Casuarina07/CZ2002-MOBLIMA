0014dbd3cc8ab3701ad8f599ea488482928e3cc0
gwynbxm <45753405+gwynbxm@users.noreply.github.com>
2019-11-15 16:14:12 +0800

    Updated DatabaseController.java
    Added in readMovieListing method.

--- MOBLIMA/src/control/DatabaseController.java
+++ MOBLIMA/src/control/DatabaseController.java
@@ -34,6 +34,74 @@ public class DatabaseController {
 		}
 
 	}
+	
+	public static ArrayList<MovieListing> readMovieListing() 
+	{
+		String filePath = "src/storage/MovieListing.csv";
+
+		BufferedReader br = null;
+		String line = ""; 
+		String cvsSplitBy = ",";
+
+		ArrayList<MovieListing> movies = new ArrayList<MovieListing>();
+
+		try {
+				br = new BufferedReader(new FileReader(filePath));
+				
+				
+				while ((line = br.readLine()) != null) 
+				{
+					String[] value = line.split(cvsSplitBy);
+
+					int movieID = Integer.parseInt(value[0]);
+					String movieTitle = value[1];
+					// movieStatus
+					MovieListing.MovieStatus movieStatus = MovieListing.MovieStatus.valueOf(value[2]);
+					String movieDirector = value[3];
+					int movieDuration = Integer.parseInt(value[4]);
+					// casts arrayList
+					ArrayList<String> casts = new ArrayList<String>();
+					String[] castsList = value[5].split(";");
+					for (String cast : castsList) 
+					{
+						casts.add(cast);
+					}
+					MovieListing.MovieGenre movieGenre = MovieListing.MovieGenre.valueOf(value[6]);
+
+					MovieListing.MovieRating movieRating = MovieListing.MovieRating.valueOf(value[7]);
+
+					String movieSynopsis = value[8];
+
+					MovieListing movie = new MovieListing(movieID, movieTitle, movieStatus, movieDirector,
+							movieDuration, casts, movieGenre, movieRating, movieSynopsis);
+					movies.add(movie);
+				}
+		
+			}
+			catch(FileNotFoundException e)
+			{
+				e.printStackTrace();
+			}
+			catch(IOException e)
+			{
+				e.printStackTrace();
+			}
+			finally
+			{
+				if (br != null) 
+				{
+					try 
+					{
+						br.close();
+					} 
+					catch (IOException e) 
+					{
+						e.printStackTrace();
+					}
+				}
+			}
+				return movies;
+	}
 
 	public static ArrayList<Cineplex> readCineplex() {
 		initBufferReader("Cineplex.csv");
