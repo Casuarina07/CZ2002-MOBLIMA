47883f3ada9a4eebd6e73a195b4fe5f94421d5ba
gwynbxm <45753405+gwynbxm@users.noreply.github.com>
2019-11-16 19:47:37 +0800

    Updated MoblimaApp.java
    Placed all codes into a do while loop.

--- MOBLIMA/src/view/MoblimaApp.java
+++ MOBLIMA/src/view/MoblimaApp.java
@@ -3,52 +3,74 @@ package view;
 import java.util.Scanner;
 
 import control.StaffController;
+import model.Staff;
 
+
+/**
+ * @author Gwyn Bong Xiao Min
+ * @author Casuarina D/O Abdul Karim
+ * @since 2019-11-01
+ */
 public class MoblimaApp {
 
 	public static void main(String[] args) {
 
+		StaffController staffCon = StaffController.getInstance();
+		
+		//load in staff info into staff.csv
+		boolean loadInStaffData = staffCon.loadStaffList();
+		
 		Scanner sc = new Scanner(System.in);
 
 		int choice;
+		
+		do {
+			System.out.println("________WELCOME TO MOBLIMA_________\n"
+							+ "                                   \n"
+							+ " 1. Login as Staff                 \n"
+							+ " 2. Continue as Guest              \n"
+							+ " 0. Exit Program                   \n"
+							+ "____________________________________\n"
+							+ " Enter your choice here: ");
+			
+			choice = sc.nextInt();
 
-		System.out.println("________WELCOME TO MOBLIMA_________\n"
-						+ "| 1. Login as Staff                 |\n"
-						+ "| 2. Continue as Guest              |\n"
-						+ "| 0. Exit Program                   |\n"
-						+ "____________________________________\n");
-		choice = sc.nextInt();
-
-		switch (choice) {
-		case 1:
-			StaffController staffCon = StaffController.getInstance();
-			StaffUI staffUi = StaffUI.getInstance();
-
-			System.out.println("Enter username: ");
-			String username = sc.nextLine();
-			System.out.println("Enter password: ");
-			String password = sc.nextLine();
-
-			boolean isLoggedIn = staffCon.validateStaffLogin(username, password);
-
-			if (isLoggedIn) {
-				staffUi.displayMenu();
-				break;
+			if(choice < 1) {
+				System.out.println("Wrong choice. Please re-enter");
+				continue;
 			} else {
-				System.out.println("The username or password entered is invalid, please try again.");
-				break;
+				switch (choice) {
+					case 1: // Login as Staff
+						System.out.println("Enter username: ");
+						String username = sc.next();
+						System.out.println("Enter password: ");
+						String password = sc.next();
+			
+						while(loadInStaffData) {
+							//verify staff login and get which staff has logged in
+							Staff getLoggedInStaff = staffCon.validateStaffLogin(username, password);
+			
+							if (getLoggedInStaff != null) {
+								System.out.println("Hello, " + getLoggedInStaff.getStaffName());
+								StaffUI staffUi = StaffUI.getInstance();
+								staffUi.displayMenu();
+								break;
+							} else {
+								System.out.println("Invalid login credentials. Please re-enter.");
+								continue;
+							}
+						}
+						break;
+					
+					case 2: // Continue as Guest
+						MovieGoerUI movieGoerUI = MovieGoerUI.getInstance();
+						movieGoerUI.displayMenu();
+						break;
+					case 0: // Exit Program
+						System.out.print("System terminating..");
+						break;
+				}
 			}
-
-		case 2:
-			// Display Guest Menu
-			MovieGoerUI movieGoerUI = MovieGoerUI.getInstance();
-			movieGoerUI.displayMenu();
-		case 0:
-			System.out.print("System terminating..");
-			break;
-		}
-
-		sc.close();
-	}
-
+		} while(choice > 2);
+	}	
 }
