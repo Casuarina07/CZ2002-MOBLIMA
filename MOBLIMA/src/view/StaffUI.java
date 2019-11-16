68d8a493c07228ccd3bcdeeac9ca5340158e3f62
gwynbxm <45753405+gwynbxm@users.noreply.github.com>
2019-11-16 19:51:40 +0800

    Update StaffUI.java
    Only menu for now. Will push in the rest once all works.

--- MOBLIMA/src/view/StaffUI.java
+++ MOBLIMA/src/view/StaffUI.java
@@ -1,14 +1,35 @@
 package view;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
+import control.DatabaseController;
+import control.SearchAndListController;
 import control.StaffController;
+import model.Cinema;
+import model.Cineplex;
 import model.MovieListing;
+import model.MovieShowing;
+import model.ShowTime;
 
+/**
+ * @author Gwyn Bong Xiao Min
+ * @since 2019-11-01
+ */
 public class StaffUI {
-
+	
+	private static Scanner sc;
 	private static StaffUI staffUI = null;
-	private Scanner sc; 
-	private MovieListing movie;
+	 
+	private static MovieListing movie = new MovieListing();
+	static MovieShowing movieShowing = new MovieShowing();
+	
+	private static ArrayList<MovieListing> movieArrayList = MovieListing.getMovieList();
+	private static ArrayList<Cineplex> cineplexArrayList = Cineplex.getCineplexList();
+	private static ArrayList<Cinema> cinemaArrayList = Cineplex.getCinemaList();
+	private static List<MovieListing> movieList = new ArrayList<MovieListing>();
+	
+	private static StaffController staffCon = StaffController.getInstance();
 	
 	private StaffUI() {
 		sc = new Scanner(System.in);
@@ -21,58 +42,49 @@ public class StaffUI {
 		return staffUI;
 	}
 	
+	/**
+	 * Display a menu with options
+	 */
 	public void displayMenu() {
 		int choice;
-		StaffController staffCon = StaffController.getInstance();
 		
 		do {
-			System.out.println("_____________ STAFF MENU ______________\n"
-								+ "|                                    |\n"
-								+ "| 1. Create Movie Listing            |\n"
-								+ "| 2. Update Movie Listing            |\n"
-								+ "| 3. Remove Movie Listing            |\n"
-								+ "| 4. Create Movie Showing            |\n"
-								+ "| 5. Update Movie Showing            |\n"
-								+ "| 6. Remove Movie Showing            |\n"
-								+ "| 7. Configure Settings	            |\n"
-								+ "| 8. Generate Sales Report           |\n"
-								+ "| 0. Logout                          |\n");
+			System.out.println("____________ STAFF MENU ____________\n"
+							+ "                                     \n"
+							+ " 1. Add New Movie		            \n"
+							+ " 2. Edit Movie Details	            \n"
+							+ " 3. Configure Settings	            \n"
+							+ " 4. Manage Sales		            	\n"
+							+ " 0. Logout                           \n"
+							+ "_____________________________________\n"
+							+ " Enter your choice here: ");
+			
 			choice = sc.nextInt();
 			
 			switch(choice) {
 				case 1:
-
-					staffCon.createMovieListing(movie);
+//					addNewMovie();
 					break;
 				case 2:
-					staffCon.updateMovieListing();
+//					editAMovie();
 					break;
 				case 3:
-					staffCon.removeMovieListing();
+					StaffConfigSystemUI staffCS = StaffConfigSystemUI.getInstance();
+					staffCS.displayMenu();
 					break;
 				case 4:
-					staffCon.createMovieShowing();
-					break;
-				case 5:
-					staffCon.updateMovieShowing();
-					break;
-				case 6:
-					staffCon.removeMovieShowing();
-					break;
-				case 7:
-					staffCon.configureSettings();
-					break;
-				case 8:
-					staffCon.generateSalesReport();
+					StaffManageTop5UI staffMT5 = StaffManageTop5UI.getInstance();
+					staffMT5.displayMenu();
 					break;
 				case 0:
-					staffCon.staffLogout();
-					break;
+					System.out.println("You have successfully logged out!");
+					return;
 				default:
 					System.out.println("Invalid choice, please try again");
 					break;
 			}
 			
-		} while(choice > 0);
+		} while(choice >= 0 || choice <= 8);
 	}
+
 }
